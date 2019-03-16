ffee62a8819db293db12d7cec4b5285f1982f867
mchmielo <matt.chmielowski@gmail.com>
2019-03-16 13:37:28 +0100

    Unit tests project added to solution. NUnit packages installed.

---
+++ ISSTracker/ISSTracker.Test/NUnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//using NUnit.Framework;
+
+namespace ISSTracker.Test
+{
+    //[TestFixture]
+    public class NUnitTest1
+    {
+        //[Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
